efd281382c90be8aae63fb3df92d148093ebbd2c
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-04-22 10:01:51 -0500

    added the routes for the pages and functionality to post a note. took that post and added it into the db.json file

--- Develop/server.js
+++ Develop/server.js
@@ -8,13 +8,42 @@ app.use(express.json());
 app.use(express.static('public'));
 const PORT = process.env.PORT || 3001;
 
+
+//home
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+//notes
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+//get the notes
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+  });
+
+//posting the notes
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+    
+        let noteInput = JSON.parse(data);
+        noteInput.push(newNote);
+        
+        
+        fs.writeFile("./db/db.json", JSON.stringify(noteInput), "utf-8", (err) => {
+            if (err) throw err;
+            console.log("note saved");
+        });
+    })
+});
+
+
+
 
 
 app.listen(PORT, () => {
