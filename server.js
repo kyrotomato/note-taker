453a7ce26a039245ac74e2cc0888ec4e238714d0
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-04-22 09:00:48 -0500

    added app.use for static file serving

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+//app.use
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
 });
 
 app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
 });
 
 app.listen(3001, () => {
