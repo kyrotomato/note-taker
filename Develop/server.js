b94e0a4566f943cacfb85183e9235ac615aa4a39
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-04-23 13:39:37 -0500

    added catch all and moved to bottom of code to prevent errors

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const {v4 : uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,10 +10,6 @@ app.use(express.static('public'));
 const PORT = process.env.PORT || 3001;
 
 
-//home
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 //notes
 app.get('/notes', function (req, res) {
@@ -23,21 +19,21 @@ app.get('/notes', function (req, res) {
 //get the notes
 app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
-  });
+});
 
 //posting the notes
 app.post("/api/notes", function (req, res) {
     var newNote = req.body;
     var idofNote = uuidv4()
-    newNote.id= idofNote;
+    newNote.id = idofNote;
     res.sendFile(path.join(__dirname, "./db/db.json"));
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
-    
+
         let noteInput = JSON.parse(data);
         noteInput.push(newNote);
-        
-        
+
+
         fs.writeFile("./db/db.json", JSON.stringify(noteInput), "utf-8", (err) => {
             if (err) throw err;
             console.log("note saved");
@@ -45,6 +41,10 @@ app.post("/api/notes", function (req, res) {
     })
 });
 
+//home
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
